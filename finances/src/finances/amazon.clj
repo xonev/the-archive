0701ef32aa2ee04966a5b226683527feb96f694e
Steven Oxley <steven.aj.oxley@gmail.com>
2020-04-20 12:29:32 -0700

    Refine unique combinations approach.

--- finances/src/finances/amazon.clj
+++ finances/src/finances/amazon.clj
@@ -38,46 +38,6 @@
   [charges items]
   )
 
-(defn- max-val-for-index
-  [n r index]
-  (+ (- n r) index))
-
-(defn- inc-needed
-  [curr-combo n r]
-  (reduce (fn [filtered index]
-              (if (< (nth curr-combo index) (max-val-for-index n r index))
-                (conj filtered index)
-                filtered))
-            []
-            (range (dec r) -1 -1)))
-
-(defn- next-combo
-  [curr-combo n]
-  (let [r (count curr-combo)
-        inc-index (first (inc-needed curr-combo n r))]
-    (if (not inc-index)
-      nil
-      (reduce (fn [new-combo index]
-                (cond
-                  (= index inc-index)
-                  (update new-combo index inc)
-
-                  (and (> index inc-index)
-                       (> index 0)
-                       (= (get new-combo index)
-                          (max-val-for-index n r index)))
-                  (assoc new-combo index (inc (get new-combo (dec index))))
-
-                  #_(and (> index inc-index)
-                         (< (get new-combo index)
-                            (max-val-for-index n r index)))
-                  #_(update new-combo index inc)
-
-                  :else
-                  new-combo))
-              curr-combo
-              (range (count curr-combo))))))
-
 (defn cart-product
   ([row-colls] (cart-product (map #(conj [] %) (first row-colls)) (rest row-colls)))
   ([product row-colls]
@@ -98,27 +58,6 @@
    (map set)
    set))
 
-(comment 5 choose 3
-   0 1 2
-   0 1 3
-   0 1 4
-   0 2 3
-   0 2 4
-   0 3 4
-   1 2 3
-   1 2 4
-   1 3 4
-   2 3 4
-   (repeatedly 3 (fn [] {:a :b}))
-   (range (dec 3) -1 -1)
-   (inc-needed [0 3 4] 5 3)
-   (next-combo [0 3 5] 6)
-   (def prices [{:price 10} {:price 15} {:price 25}])
-   (def cart-product-args [prices prices])
-   (unique-combos prices)
-   (unique-combos (cart-product cart-product-args))
-   )
-
 (def orders-file-path (partial file-type-path "orders"))
 (def items-file-path (partial file-type-path "items"))
 (def refunds-file-path (partial file-type-path "refunds"))
@@ -136,4 +75,8 @@
   (concat [[1 2 3]] [[4 5 6] [7 8 9]] nil [[10 11 12]])
   (all-combos items)
   (re-find #"/\d{4}-\w+$" "/a/b/c/2020text")
+
+   (def prices [{:price 10} {:price 15} {:price 25}])
+   (def cart-product-args [prices prices])
+   (unique-combos prices)
   )

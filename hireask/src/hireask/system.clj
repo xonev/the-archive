0e3c31594419df62576a5f35e010543a2762a0a1
Steven Oxley <steven.aj.oxley@gmail.com>
2021-01-11 22:12:51 -0800

    Update spacemacs config and hireask.

--- hireask/src/hireask/system.clj
+++ hireask/src/hireask/system.clj
@@ -4,7 +4,7 @@
             [hireask.query-api :as q]
             [integrant.core :as ig]))
 
-(defmethod ig/init-key :db [_ config]
+(defmethod ig/init-key :the-db [_ config]
   (db/init config))
 
 (defmethod ig/init-key :query-api [_ config]
@@ -16,6 +16,8 @@
       io/resource
       slurp
       ig/read-string))
+
+(ig/read-string (slurp (io/resource "system.edn")))
 
 (defn start
   [system]
